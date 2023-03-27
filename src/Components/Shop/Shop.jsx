2506458f3d0e98c4f4d13e5e17ea405b4f6e6fd3
SQLAdmin <abuyeahia24@gmail.com>
2023-03-28 02:43:08 +0600

    added some cart

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Products/Product';
 import './Shop.css'
@@ -10,11 +11,17 @@ const Shop = () => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
+    },[]);
+
+    useEffect(()=>{
+        const stored = getShoppingCart();
+        console.log(stored);
     },[])
 
     const handleAddToCart = (product) =>{
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
        }
     // console.log(products);
     return (
