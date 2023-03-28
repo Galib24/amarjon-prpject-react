1ba390e268f87578ce1a384f88205fec8709919c
SQLAdmin <abuyeahia24@gmail.com>
2023-03-28 16:03:54 +0600

    fixed ui for show data perfect

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -7,16 +7,22 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping + product.shipping;
+        // if(product.quantity === 0){
+        //     product.quantity =1;
+        // }
+        // product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping * product.quantity;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice*7/100;
     const GrandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'> 
                 <h4>order-summery</h4>
-                <p>selected Items: {cart.length}</p>
+                <p>selected Items: {quantity}</p>
                 <p>Total Price: ${totalPrice}</p>
                 <p>Total Shipping: ${totalShipping}</p>
                 <p>Tax: ${tax.toFixed(2)}</p>
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -36,9 +36,28 @@ const Shop = () => {
     }, [products])
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+           
+
+        let newCart = [];
+
+        // const newCart = [...cart, product];
+        // if product dosen't exit in the cart, then set quantity = 1
+        // if exist update quantity by 1.
+
+         const exists = cart.find(pd => pd.id === product.id);
+         if(!exists){
+            product.quantity =1;
+            newCart =[...cart, product]
+         }
+         else{
+            exists.quantity = exists.quantity +1;
+            const remaining = cart.filter(pd => pd.id  !== product.id);
+            newCart = [...remaining, exists]
+         }
+
         setCart(newCart);
         addToDb(product.id)
+        
     }
     // console.log(products);
     return (
