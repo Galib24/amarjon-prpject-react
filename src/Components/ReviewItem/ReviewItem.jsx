a014eb0f5a1bd8de83fc968ab2142c8dc85aaa67
SQLAdmin <abuyeahia24@gmail.com>
2023-04-05 16:40:42 +0600

    order page done with css

--- src/Components/ReviewItem/ReviewItem.jsx
+++ src/Components/ReviewItem/ReviewItem.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './ReviewItem.css'
-const ReviewItem = ({product}) => {
-    const {id, img, price, name, quantity} = product;
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+const ReviewItem = ({ product }) => {
+    const { id, img, price, name, quantity } = product;
     return (
         <div className='review-item'>
-           <img src={img} alt="" />
+            <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
+                <p></p>
+            </div>
+        <button className='btn-delete'> 
+        <FontAwesomeIcon className='delete-icon' icon={faTrashAlt}></FontAwesomeIcon>
+        </button>
         </div>
     );
 };
